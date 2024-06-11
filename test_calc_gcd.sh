ebf551c8145a05404216da24ab14b81cd339bcff
Keisuke Yoshida <z24058ky@aiit.ac.jp>
2024-06-11 16:32:02 +0000

    test_calc_gcd.sh:test1 run

--- test_calc_gcd.sh
+++ test_calc_gcd.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# テスト1:引数の数が0
-#./calc_gcd.sh
+# test1:引数の数が0
+./calc_gcd.sh
 
-# テスト2:引数の数が1
-./calc_gcd.sh 10
+# test2:引数の数が1
+#./calc_gcd.sh 10
+
+# test3:引数の数が2
+#./calc_gcd.sh 10 20
+
+# test4:引数の数が3
+#./calc_gcd.sh 10 20 30
 
 if [ $? -eq 0 ]; then
     echo "$0 is pass."

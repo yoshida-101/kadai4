b4aca4670ca43b02ce5b8e057184b4c508bcfe25
Keisuke Yoshida <z24058ky@aiit.ac.jp>
2024-06-11 16:59:16 +0000

    Change calc_gcd.sh and test_calc_gcd.sh:test5 run

--- calc_gcd.sh
+++ calc_gcd.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# 終了ステータスは以下の通りとする
+# 0: 正常終了
+# 1: 引数の数が2ではない
+# 2: 引数が自然数ではない
+# 3: 引数が整数の最大値を超えている
+
 CHECK_ARG_COUNT () {
     if [ $# -ne 2 ]; then
         echo "引数を2つ入力してください"
@@ -7,6 +13,31 @@ CHECK_ARG_COUNT () {
     fi
 }
 
-# Main
 
+CHECK_IS_NATURAL_NUMBER () {
+    # exprは解が0のときに終了ステータス1を返す
+    # exprは引数が整数でないときに終了ステータス2を返す
+    expr $1 + 0 >/dev/null 2>&1
+
+    if [ $? -ne 0 ]; then
+        echo "$1 は自然数ではありません"
+        exit 2
+    # 整数の最大値を超えているか確認する
+    # オーバーフローして文字列として一致しないことを確認する
+    elif [ $(( $1 + 0 )) != $1 ]; then
+        echo "整数の最大値を超えています"
+        exit 3
+    # Check if it's a negative number
+    elif [ $1 -lt 0 ]; then
+        echo "$1 は自然数ではありません"
+        exit 2
+    fi
+}
+
+
+# Main
 CHECK_ARG_COUNT $@
+
+for i in "$@"; do
+    CHECK_IS_NATURAL_NUMBER ${i}
+done
--- test_calc_gcd.sh
+++ test_calc_gcd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+## CHECK_ARG_COUNTのテスト
 # test1:引数の数が0
 #./calc_gcd.sh
 
@@ -10,7 +11,26 @@
 #./calc_gcd.sh 10 20
 
 # test4:引数の数が3
-./calc_gcd.sh 10 20 30
+#./calc_gcd.sh 10 20 30
+
+## CHECK_IS_NATURAL_NUMBERのテスト
+# test5:引数に文字が含まれる
+./calc_gcd.sh 10 テスト
+
+# test6:引数に0が含まれる
+#./calc_gcd.sh 10 0
+
+# test7:引数に小数が含まれる
+#./calc_gcd.sh 10 2.5
+
+# test8:引数が整数の最大値を超えている
+#./calc_gcd.sh 10 9223372036854775808
+
+# test9:引数に負の数が含まれる
+#./calc_gcd.sh 10 -20
+
+# test10:引数が2つとも自然数
+#./calc_gdc.sh 10 20
 
 if [ $? -eq 0 ]; then
     echo "$0 is pass."
